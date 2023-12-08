3a0e46f59e009e31cdd26b366d22c2eff0454331
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-12-08 11:42:17 +0300

    feat: Implement right bar components and styles

---
+++ src/components/RightBar.jsx
@@ -0,0 +1,206 @@
+const RightBar = () => {
+  return (
+    <div className="flex-[3] sticky top-[4.375rem] overflow-scroll scroll-bar space-y-5 pb-5 bg-secondary">
+      <div className="p-5 space-y-5 shadow-lg bg-primary">
+        <p className="capitalize text-gray-500 font-semibold">
+          suggestions for you
+        </p>
+        <ul className="space-y-5">
+          <li className="flex justify-between items-center">
+            <div className="flex items-center gap-2.5">
+              <img
+                className="w-10 h-10 rounded-full"
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="Jane Doe profile image"
+              />
+              <span className="text-primary font-medium">Jane Doe</span>
+            </div>
+            <div className="space-x-2">
+              <button className="px-2 text-white rounded-md bg-blue-600">
+                follow
+              </button>
+              <button className="px-2 text-white rounded-md bg-red-600">
+                dismiss
+              </button>
+            </div>
+          </li>
+          <li className="flex justify-between items-center">
+            <div className="flex items-center gap-2.5">
+              <img
+                className="w-10 h-10 rounded-full"
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="Jane Doe profile image"
+              />
+              <span className="text-primary font-medium">Jane Doe</span>
+            </div>
+            <div className="space-x-2">
+              <button className="px-2 text-white rounded-md bg-blue-600">
+                follow
+              </button>
+              <button className="px-2 text-white rounded-md bg-red-600">
+                dismiss
+              </button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="p-5 space-y-5 shadow-lg bg-primary">
+        <p className="capitalize text-gray-500 font-semibold">
+          latest activities
+        </p>
+        <ul className="space-y-5">
+          <li className="flex justify-between items-center">
+            <div className="flex items-center gap-2.5">
+              <img
+                className="w-10 h-10 rounded-full"
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="Jane Doe profile image"
+              />
+              <p className="text-secondary">
+                <span>Jane Doe</span> changed there profile picture
+              </p>
+            </div>
+            <span className="text-primary font-medium">1 min ago</span>
+          </li>
+          <li className="flex justify-between items-center">
+            <div className="flex items-center gap-2.5">
+              <img
+                className="w-10 h-10 rounded-full"
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="Jane Doe profile image"
+              />
+              <p className="text-secondary">
+                <span>Jane Doe</span> changed there profile picture
+              </p>
+            </div>
+            <span className="text-primary font-medium">1 min ago</span>
+          </li>
+          <li className="flex justify-between items-center">
+            <div className="flex items-center gap-2.5">
+              <img
+                className="w-10 h-10 rounded-full"
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="Jane Doe profile image"
+              />
+              <p className="text-secondary">
+                <span>Jane Doe</span> changed there profile picture
+              </p>
+            </div>
+            <span className="text-primary font-medium">1 min ago</span>
+          </li>
+          <li className="flex justify-between items-center">
+            <div className="flex items-center gap-2.5">
+              <img
+                className="w-10 h-10 rounded-full"
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="Jane Doe profile image"
+              />
+              <p className="text-secondary">
+                <span>Jane Doe</span> changed there profile picture
+              </p>
+            </div>
+            <span className="text-primary font-medium">1 min ago</span>
+          </li>
+        </ul>
+      </div>
+      <div className="p-5 space-y-5 shadow-lg bg-primary">
+        <p className="capitalize text-gray-500 font-semibold">online friends</p>
+        <ul className="space-y-5">
+          <li className="flex justify-between items-center">
+            <div className="flex items-center gap-2.5">
+              <div className="relative">
+                <img
+                  className="w-10 h-10 rounded-full"
+                  src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt="Jane Doe profile image"
+                />
+                <div className="absolute top-0 right-0 w-3 h-3 rounded-full bg-green-500"></div>
+              </div>
+              <span className="text-primary font-medium">Jane Doe</span>
+            </div>
+          </li>
+          <li className="flex justify-between items-center">
+            <div className="flex items-center gap-2.5">
+              <div className="relative">
+                <img
+                  className="w-10 h-10 rounded-full"
+                  src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt="Jane Doe profile image"
+                />
+                <div className="absolute top-0 right-0 w-3 h-3 rounded-full bg-green-500"></div>
+              </div>
+              <span className="text-primary font-medium">Jane Doe</span>
+            </div>
+          </li>
+          <li className="flex justify-between items-center">
+            <div className="flex items-center gap-2.5">
+              <div className="relative">
+                <img
+                  className="w-10 h-10 rounded-full"
+                  src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt="Jane Doe profile image"
+                />
+                <div className="absolute top-0 right-0 w-3 h-3 rounded-full bg-green-500"></div>
+              </div>
+              <span className="text-primary font-medium">Jane Doe</span>
+            </div>
+          </li>
+          <li className="flex justify-between items-center">
+            <div className="flex items-center gap-2.5">
+              <div className="relative">
+                <img
+                  className="w-10 h-10 rounded-full"
+                  src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt="Jane Doe profile image"
+                />
+                <div className="absolute top-0 right-0 w-3 h-3 rounded-full bg-green-500"></div>
+              </div>
+              <span className="text-primary font-medium">Jane Doe</span>
+            </div>
+          </li>
+          <li className="flex justify-between items-center">
+            <div className="flex items-center gap-2.5">
+              <div className="relative">
+                <img
+                  className="w-10 h-10 rounded-full"
+                  src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt="Jane Doe profile image"
+                />
+                <div className="absolute top-0 right-0 w-3 h-3 rounded-full bg-green-500"></div>
+              </div>
+              <span className="text-primary font-medium">Jane Doe</span>
+            </div>
+          </li>
+          <li className="flex justify-between items-center">
+            <div className="flex items-center gap-2.5">
+              <div className="relative">
+                <img
+                  className="w-10 h-10 rounded-full"
+                  src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt="Jane Doe profile image"
+                />
+                <div className="absolute top-0 right-0 w-3 h-3 rounded-full bg-green-500"></div>
+              </div>
+              <span className="text-primary font-medium">Jane Doe</span>
+            </div>
+          </li>
+          <li className="flex justify-between items-center">
+            <div className="flex items-center gap-2.5">
+              <div className="relative">
+                <img
+                  className="w-10 h-10 rounded-full"
+                  src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt="Jane Doe profile image"
+                />
+                <div className="absolute top-0 right-0 w-3 h-3 rounded-full bg-green-500"></div>
+              </div>
+              <span className="text-primary font-medium">Jane Doe</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default RightBar;
