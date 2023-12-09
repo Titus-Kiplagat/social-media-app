09126a45c6337e5c872f2c47801ecaa132076a7c
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-12-09 21:18:45 +0300

    Implement post listing in posts component

---
+++ src/components/Posts.jsx
@@ -0,0 +1,42 @@
+import Post from "./Post";
+
+const Posts = () => {
+  const posts = [
+    {
+      id: 1,
+      name: "John Doe",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+      userId: 2,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.",
+    },
+    {
+      id: 3,
+      name: "Jane Doe",
+      userId: 3,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.",
+      img: "https://images.pexels.com/photos/19404174/pexels-photo-19404174/free-photo-of-cosy-christmas.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-5">
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
